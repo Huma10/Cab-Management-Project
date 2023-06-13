a125999d47898a523f070931df9ef88275682927
Huma10 <khanhuma1021994@gmail.com>
2023-06-13 20:23:25 +0530

    created Project structure

---
+++ src/main/java/com/cabmanagement/CabManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cabmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabManagementProjectApplication.class, args);
+	}
+
+}
